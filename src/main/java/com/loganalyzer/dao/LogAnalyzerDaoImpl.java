6f034987aff0f072f1e58af0a121fcffa09c5fd5
Gagan Gupta <gagan.gupta@AASPL-BRD.COM>
2018-12-13 00:18:50 +0530

    on the fly unzip: gz and zip files

--- src/main/java/com/loganalyzer/dao/LogAnalyzerDaoImpl.java
+++ src/main/java/com/loganalyzer/dao/LogAnalyzerDaoImpl.java
@@ -13,6 +13,9 @@ import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -24,6 +27,9 @@ import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @Repository
 @Qualifier("InitializedLogs")
@@ -59,35 +65,52 @@ public class LogAnalyzerDaoImpl implements LogAnalyzerDao{
     public void initialize() {
 
         List<File> list = new ArrayList<File>();
+        String[] compressedList = new String[] {"zip", "gz"};
+        Collection<File> compressedFiles = FileUtils.listFiles(new File(logsPath), compressedList, true);
+        for (File file1 : compressedFiles){
+            String ext = FilenameUtils.getExtension(file1.getPath());
+            if (compressedList[0].equals(ext)){
+                String inputFile = file1.getAbsolutePath();
+                String outputFolder = inputFile.substring(0, inputFile.lastIndexOf("\\"));
+                unZipIt(inputFile, outputFolder);
+            }else if (compressedList[1].equals(ext)){
+                String inputFile = file1.getAbsolutePath();
+                String outputFile = inputFile.substring(0, inputFile.lastIndexOf("."));
+                gunzipIt(inputFile, outputFile);
+            }
+        }
+
         String[] extensions = new String[] { "log" };
         Collection<File> array = FileUtils.listFiles(new File(logsPath), extensions, true);
 
         String format;
-        for (File file1 : array){
+        for (File file : array){
 
-            String ext = FilenameUtils.getExtension(file1.getPath());
-            String fileName = Utility.getFileName(file1.getPath());
+            String fileName = Utility.getFileName(file.getPath());
             boolean found = filesWithFormatPatternNoLocation
                     .stream()
                     .filter((string) -> fileName.contains(string)).findFirst().isPresent();
             if (found){
-                format = formatPatternNoLocation;
+                generator(formatPatternNoLocation, file);
             }else {
-                format = formatPattern;
-            }
-
-            if ("log".equals(ext)){
-
-                LogEventsGenerator receiver = new LogEventsGenerator(logs);
-                receiver.setTimestampFormat(timestamp);
-                receiver.setLogFormat(format);
-                receiver.setFileURL("file:///" + file1.getAbsolutePath());
-                receiver.setTailing(false);
-                receiver.activateOptions();
-
+                found = filesWithFormatPattern
+                    .stream()
+                    .filter((string) -> fileName.contains(string)).findFirst().isPresent();
+                if (found) {
+                    generator(formatPattern, file);
+                }
             }
         }
 
+    }
+
+    private void generator(String format, File file){
+        LogEventsGenerator receiver = new LogEventsGenerator(logs);
+        receiver.setTimestampFormat(timestamp);
+        receiver.setLogFormat(format);
+        receiver.setFileURL("file:///" + file.getAbsolutePath());
+        receiver.setTailing(false);
+        receiver.activateOptions();
     }
 
     @Override
@@ -187,4 +210,79 @@ public class LogAnalyzerDaoImpl implements LogAnalyzerDao{
         }
     }
 
+    public void gunzipIt(String inputFile, String outputFile){
+
+        byte[] buffer = new byte[1024];
+
+        try{
+
+            GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(inputFile));
+            FileOutputStream out = new FileOutputStream(outputFile);
+
+            int len;
+            while ((len = gzis.read(buffer)) > 0) {
+                out.write(buffer, 0, len);
+            }
+
+            gzis.close();
+            out.close();
+
+            System.out.println("Done");
+
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void unZipIt(String zipFile, String outputFolder){
+
+        byte[] buffer = new byte[1024];
+
+        try{
+
+            //create output directory is not exists
+            File folder = new File(outputFolder);
+            if(!folder.exists()){
+                folder.mkdir();
+            }
+
+            //get the zip file content
+            ZipInputStream zis =
+                    new ZipInputStream(new FileInputStream(zipFile));
+            //get the zipped file list entry
+            ZipEntry ze = zis.getNextEntry();
+
+            while(ze!=null){
+
+                String fileName = ze.getName();
+                File newFile = new File(outputFolder + File.separator + fileName);
+
+                System.out.println("file unzip : "+ newFile.getAbsoluteFile());
+
+                //create all non exists folders
+                //else you will hit FileNotFoundException for compressed folder
+                new File(newFile.getParent()).mkdirs();
+
+                FileOutputStream fos = new FileOutputStream(newFile);
+
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+
+                fos.close();
+                ze = zis.getNextEntry();
+            }
+
+            zis.closeEntry();
+            zis.close();
+
+            System.out.println("Done");
+
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
 }
+
